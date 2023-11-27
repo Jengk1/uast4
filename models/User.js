7d1c2a78a1a5f9bceec2a2e959ff6db1e1e0218c
Jengk1 <147509507+Jengk1@users.noreply.github.com>
2023-11-27 18:39:59 +0700

    UASAE

--- models/User.js
+++ models/User.js
@@ -1,36 +1,12 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema ({ //memanggil module dan membuat schema
-      nama_tempat_pkl: {
+      nama_mapel: {
         type: String,
-        required:[true, 'silahkan isikan nama Tempat'],
+        required:[true, 'silahkan isikan mapel'],
         unique: true,
         allowNull: false,
       },
-      alamat_tempat_pkl: {
-        type: String,
-        required:[true, 'silahkan isikan alamat'],
-        unique: true,
-        allowNull: false,
-      },
-      nama_pekerja: {
-        type: String,
-        required:[true, 'silahkan isikan pegawai'],
-        unique: true,
-        allowNull: false,
-      },
-      jabatan: {
-        type: String,
-        required:[true, 'silahkan isikan jabatan'],
-        unique: true,
-        allowNull: false,
-      },
-      nama_siswa_pkl: {
-        type: String,
-        required:[true, 'isikan nama siswa pkl'],
-        unique: true,
-        allowNull: true,
-      },
-    });   
+      });   
 
 module.exports = mongoose.model('User', UserSchema);
